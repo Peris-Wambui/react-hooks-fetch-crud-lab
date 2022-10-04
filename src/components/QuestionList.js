345032ff42afa47cd522301a0f014590bd685069
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-10-04 12:47:57 -0400

    done

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+// import QuestionForm from "./QuestionForm";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+
+function QuestionList({onUpdate}) {
+  const [questions,setQuestions]= useState ([])
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then((r)=>r.json())
+    .then((data)=>setQuestions(data));
+  },[]);
+
+  function handleDeleteQuestion(deletedQuestion){
+    const updateQuestions = questions.filter((question) => question.id !== deletedQuestion.id);
+    setQuestions(updateQuestions);
+   
+  }
+
+  function addQuestion(item){
+    setQuestions([...questions, item]);
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {/* <QuestionForm onAddItem={addQuestion}/> */}
+      <ul>{/* display QuestionItem components here after fetching */}
+        {questions.map((data)=> 
+        <QuestionItem
+        key={data.id}
+        question={data}
+        onAddItem ={addQuestion}
+        onDeleteQuestion ={handleDeleteQuestion}
+        onUpdate={onUpdate}
+        />
+        
+        )}
+      </ul>
     </section>
   );
 }
